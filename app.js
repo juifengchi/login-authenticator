a81cc464cd48bb7e2fd48c5f31230cfdcc4c42c4
ravenera0317 <r6090408@gmail.com>
2021-07-16 17:20:19 +0800

    Add successful login function

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const checkAccount = require('./check_account')
+
 const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/login', (req, res) => {
+  const firstName = checkAccount(req.body)
+  res.render('login', { firstName })
+})
+
+app.post('/login', (req, res) => {
   res.render('index')
 })
 
---
+++ check_account.js
@@ -0,0 +1,35 @@
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+function checkAccount (login) {
+  const loginUser = users.find(user => user.email === login.email && user.password === login.password)
+  if (loginUser.length === 0) return 'Email or password is incorrect.'
+  return loginUser.firstName
+}
+
+module.exports = checkAccount
